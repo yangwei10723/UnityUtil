c3e68891c238814fdf694f26be787e8c04775d7c
yangwei <1047628463@qq.com>
2017-07-15 08:43:16 +0800

    add monobehaviour cache script

---
+++ Scripts/XMonobehaviourCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class XMonoBehaviourCache : MonoBehaviour
+{
+    private bool _cachedName;
+    private string _goName;
+    public string goName
+    {
+        get
+        {
+            if (!_cachedName)
+            {
+                _goName = gameObject.name;
+                _cachedName = true;
+            }
+            return _goName;
+        }
+        set
+        {
+            _goName = value;
+            gameObject.name = value;
+            _cachedName = true;
+        }
+    }
+
+    private bool _cachedTag;
+    private string _goTag;
+    public string goTag
+    {
+        get
+        {
+            if (!_cachedTag)
+            {
+                _goTag = gameObject.tag;
+                _cachedTag = true;
+            }
+            return _goTag;
+        }
+        set
+        {
+            _goTag = value;
+            gameObject.tag = value;
+            _cachedTag = true;
+        }
+    }
+}
+
